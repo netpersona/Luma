6333c63286ab0ae3199dbbab7f51418574aad963
netpersona <csimmwork@gmail.com>
2025-12-04 19:56:43 -0600

    Delete client/pages directory

--- client/pages/opds.tsx
+++
@@ -1,9 +0,0 @@
-import { OpdsBrowser } from "@/components/OpdsBrowser";
-
-export default function OpdsPage() {
-  return (
-    <div className="container mx-auto p-6" data-testid="page-opds">
-      <OpdsBrowser />
-    </div>
-  );
-}
